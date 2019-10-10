05c59964df14cae294d965fc8271abff21a8d002
wayne-dev <tuanphuongdtvn@gmail.com>
2019-10-10 18:44:43 +0700

    update style

---
+++ public/js/editor-script.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function(){
+    $("table#register").each(function(i, v){
+        var tbody = $(this).find('tbody');
+        var thead = $(this).find('thead');
+        var header = [];
+        thead.find('th').each(function(i, v) {
+            header[i] = $(this).text();
+        });
+        tbody.find('tr').each(function(i, v) {
+            var myRow = $(this);
+            myRow.find('td').each(function(j) {
+                $(this).attr('data-th', header[j]);
+            });
+        });
+    });
+});
